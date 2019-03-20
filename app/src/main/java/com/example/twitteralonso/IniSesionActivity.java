f05b1becb839402a9bbd1626d259f755d995b583
bojackhorseman0309 <34991755+bojackhorseman0309@users.noreply.github.com>
2019-03-20 12:28:03 -0600

    Inicio

---
+++ app/src/main/java/com/example/twitteralonso/IniSesionActivity.java
@@ -0,0 +1,20 @@
+package com.example.twitteralonso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class IniSesionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ini_sesion);
+    }
+
+    public void iniciarSesionVerifica(View view){
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/twitteralonso/RegistrarseActivity.java
@@ -0,0 +1,18 @@
+package com.example.twitteralonso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class RegistrarseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrarse);
+    }
+
+    public void registroR(View view){
+
+    }
+}
